9aee0b6f18ade45b2c05e21d416bee44f4f322b2
Erin <thompsonerin42@gmail.com>
2018-09-04 13:23:11 -0400

    updated losant-file to async"

--- losant-files.js
+++ losant-files.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 const program = require('commander');
 const c = require('chalk');
 const losant = require('losant-rest');
@@ -34,100 +33,94 @@ program
     const config = loadConfig(command.config);
     const api = losant.createClient({ accessToken: config.apiToken });
     const meta = loadLocalMeta('files') || {};
-
-    api.files
-      .get({ applicationId: config.applicationId })
-      .then((files) => {
-        let items = files.items;
-        // filter out files that don't match file pattern
-        items = items.filter((file) => {
-          if (file.type === 'directory') { return false; }
-          if (pattern) {
-            if (minimatch(file.parentDirectory + file.name, pattern)) {
-              return true;
-            }
-            return false;
-          } else {
+    try {
+      const files = await api.files.get({ applicationId: config.applicationId });
+      let items = files.items;
+      // filter out files that don't match file pattern
+      items = items.filter((file) => {
+        if (file.type === 'directory') { return false; }
+        if (pattern) {
+          if (minimatch(file.parentDirectory + file.name, pattern)) {
             return true;
           }
-        });
-        // map files to id
-        const filesById = {};
-        items.forEach((item) => {
-          filesById[item.id] = item;
-        });
-        // grab the local status and map to ids
-        const localStatus = getLocalStatus('files', '/**/*.*', 'files');
-        const localStatusById = {};
-        const newLocalFiles = new Set();
-        localStatus.forEach((item) => {
-          if (item.id) {
-            localStatusById[item.id] = item;
-          } else {
-            newLocalFiles.add(item.file);
-          }
-        });
-        // iterate over remote status and perform the appropriate action
-        const remoteStatus = getRemoteStatus('files', items, 'files${parentDirectory}${name}'); // eslint-disable-line no-template-curly-in-string
-        if (command.dryRun) {
-          log('DRY RUN');
+          return false;
+        } else {
+          return true;
         }
-        remoteStatus.forEach((item) => {
-          logProcessing(item.file);
-          // if forcing the update ignore conflicts and local modifications
-          if (!command.force) {
-            if (item.status === 'unmodified') {
-              logResult('unmodified', item.file);
-              return;
-            }
-            // TODO implement conflict detection
-            // if ((localStatusById[item.id] && localStatusById[item.id].status !== 'unmodified') || newLocalFiles.has(item.file)) {
-            //   logResult('conflict', item.file, 'red')
-            //   return
-            // }
-          }
-          if (item.status === 'deleted') {
-            if (!command.dryRun) {
-              if (fs.existsSync(item.file)) {
-                fs.unlinkSync(item.file);
-              }
-              delete meta[item.file];
-            }
-            logResult('deleted', item.file, 'yellow');
-          } else {
-            if (!command.dryRun) {
-              const file = filesById[item.id];
-              const mtime = new Date(item.remoteModTime);
-              mkdirp.sync(path.dirname(item.file));
-              const res = request('GET', file.url);
-              if (res.statusCode !== 200) {
-                throw new Error(`${item.file} (${res.statusCode}: ${file.url})`);
-              }
-              const body = res.getBody();
-              fs.writeFileSync(item.file, res.getBody());
-              meta[item.file] = {
-                id: item.id,
-                md5: checksum(body),
-                remoteTime: mtime.getTime(),
-                localTime: new Date().getTime()
-              };
-            }
-            logResult('downloaded', item.file, 'green');
-          }
-        });
-      })
-      .then(() => {
-        saveLocalMeta('files', meta);
-      })
-      .catch((e) => {
-        try {
-          saveLocalMeta('files', meta);
-        } catch (err) {
-          logError(err);
-        }
-        logError(e);
-        // console.log(e)
       });
+      // map files to id
+      const filesById = {};
+      items.forEach((item) => {
+        filesById[item.id] = item;
+      });
+      // grab the local status and map to ids
+      const localStatus = getLocalStatus('files', '/**/*.*', 'files');
+      const localStatusById = {};
+      const newLocalFiles = new Set();
+      localStatus.forEach((item) => {
+        if (item.id) {
+          localStatusById[item.id] = item;
+        } else {
+          newLocalFiles.add(item.file);
+        }
+      });
+      // iterate over remote status and perform the appropriate action
+      const remoteStatus = getRemoteStatus('files', items, 'files${parentDirectory}${name}'); // eslint-disable-line no-template-curly-in-string
+      if (command.dryRun) {
+        log('DRY RUN');
+      }
+      remoteStatus.forEach((item) => {
+        logProcessing(item.file);
+        // if forcing the update ignore conflicts and local modifications
+        if (!command.force) {
+          if (item.status === 'unmodified') {
+            logResult('unmodified', item.file);
+            return;
+          }
+          // TODO implement conflict detection
+          // if ((localStatusById[item.id] && localStatusById[item.id].status !== 'unmodified') || newLocalFiles.has(item.file)) {
+          //   logResult('conflict', item.file, 'red')
+          //   return
+          // }
+        }
+        if (item.status === 'deleted') {
+          if (!command.dryRun) {
+            if (fs.existsSync(item.file)) {
+              fs.unlinkSync(item.file);
+            }
+            delete meta[item.file];
+          }
+          logResult('deleted', item.file, 'yellow');
+        } else {
+          if (!command.dryRun) {
+            const file = filesById[item.id];
+            const mtime = new Date(item.remoteModTime);
+            mkdirp.sync(path.dirname(item.file));
+            const res = request('GET', file.url);
+            if (res.statusCode !== 200) {
+              throw new Error(`${item.file} (${res.statusCode}: ${file.url})`);
+            }
+            const body = res.getBody();
+            fs.writeFileSync(item.file, res.getBody());
+            meta[item.file] = {
+              id: item.id,
+              md5: checksum(body),
+              remoteTime: mtime.getTime(),
+              localTime: new Date().getTime()
+            };
+          }
+          logResult('downloaded', item.file, 'green');
+        }
+      });
+      return saveLocalMeta('files', meta);
+    } catch (err) {
+      try {
+        saveLocalMeta('files', meta);
+      } catch (error) {
+        logError(error);
+      }
+      logError(err);
+    }
   });
 
 program
@@ -136,132 +129,126 @@ program
   .option('-c, --config <file>', 'config file to run the command with. (default: "losant.yml")')
   .option('-d, --dir <dir>', 'directory to run the command in. (default: current directory)')
   .option('--dry-run', 'display actions but do not perform them')
-  .action((pattern, command) => {
+  .action(async (pattern, command) => {
     if (command.dir) {
       process.chdir(command.dir);
     }
     const config = loadConfig(command.config);
     const api = losant.createClient({ accessToken: config.apiToken });
     const meta = loadLocalMeta('files') || {};
-
-    api.files
-      .get({ applicationId: config.applicationId })
-      .then((files) => {
-        const items = files.items;
-        // grab remote status and map to file
-        const remoteStatus = getRemoteStatus('files', items, 'files${parentDirectory}${name}'); // eslint-disable-line no-template-curly-in-string
-        const remoteStatusById = {};
-        remoteStatus.forEach((item) => {
-          if (item.id) {
-            remoteStatusById[item.id] = item;
-          }
-        });
-        // iterate over local status and perform the appropriate action
-        const localStatus = getLocalStatus('files', `/${pattern || '**/*'}`, 'files');
-        if (command.dryRun) {
-          log('DRY RUN');
+    try {
+      const files = await api.files.get({ applicationId: config.applicationId })
+      const items = files.items;
+      // grab remote status and map to file
+      const remoteStatus = getRemoteStatus('files', items, 'files${parentDirectory}${name}'); // eslint-disable-line no-template-curly-in-string
+      const remoteStatusById = {};
+      remoteStatus.forEach((item) => {
+        if (item.id) {
+          remoteStatusById[item.id] = item;
         }
-        return Promise.all(localStatus.map((item) => {
-          logProcessing(item.file);
-          // if forcing the update ignore conflicts and remote modifications
-          if (!command.force) {
-            if (item.status === 'unmodified') {
-              logResult('unmodified', item.file);
-              return;
-            }
-            // TODO implement conflict detection
-            // if ((remoteStatusById[item.id] && remoteStatusById[item.id].status !== 'unmodified')) {
-            //   logResult('conflict', item.file, 'red')
-            //   return Promise.resolve()
-            // }
+      });
+      // iterate over local status and perform the appropriate action
+      const localStatus = getLocalStatus('files', `/${pattern || '**/*'}`, 'files');
+      if (command.dryRun) {
+        log('DRY RUN');
+      }
+      return Promise.all(localStatus.map((item) => {
+        logProcessing(item.file);
+        // if forcing the update ignore conflicts and remote modifications
+        if (!command.force) {
+          if (item.status === 'unmodified') {
+            logResult('unmodified', item.file);
+            return;
           }
-          if (item.status === 'deleted') {
-            if (!command.dryRun) {
-              return api.files
-                .delete({ applicationId: config.applicationId,  fileId: item.id })
-                .then(() => {
-                  delete meta[item.file];
-                  logResult('deleted', item.file, 'yellow');
-                  return Promise.resolve();
-                });
-            }
-            logResult('deleted', item.file, 'yellow');
-            return Promise.resolve();
-          } else {
-            if (!command.dryRun) {
-              let action;
-              const body = fs.readFileSync(item.file);
-              if (item.id) {
-                action = api.file
-                  .patch({
-                    applicationId: config.applicationId,
-                    fileId: item.id,
-                    file:  {
-                      fileSize: item.size
-                    }
-                  });
-              } else {
-                const pathParts = item.file.split(path.sep);
-                action = api.files
-                  .post({
-                    applicationId: config.applicationId,
-                    file: {
-                      name: item.name,
-                      parentDirectory: pathParts.slice(1, -1).join(path.sep),
-                      type: 'file',
-                      fileSize: item.size,
-                      contentType: mimeTypes.lookup(item.file)
-                    }
-                  });
-              }
-              return action.then((file) => {
-                return new Promise((resolve, reject) => {
-                  const fd = new FormData();
-                  Object.keys(file.upload.fields).forEach((key) => {
-                    if (key !== 'bucket') {
-                      fd.append(key, file.upload.fields[key]);
-                    }
-                  });
-                  fd.append('file', body);
-                  fd.submit(file.upload.url, (err, res) => {
-                    // const body = [];
-                    if (err) {
-                      return reject(err);
-                    }
-                    res.on('data', (chunk) => {
-                      body.push(chunk);
-                    });
-                    res.on('end', () => {
-                      return resolve(file);
-                    });
-                  });
-                });
-              }).then((file) => {
-                const mtime = new Date(file.lastUpdated);
-                //fs.writeFileSync(item.file, body)
-                meta[item.file] = {
-                  id: file.id,
-                  md5: checksum(body),
-                  remoteTime: mtime.getTime(),
-                  localTime: item.localModTime * 1000
-                };
-                logResult('uploaded', item.file, 'green');
+          // TODO implement conflict detection
+          // if ((remoteStatusById[item.id] && remoteStatusById[item.id].status !== 'unmodified')) {
+          //   logResult('conflict', item.file, 'red')
+          //   return Promise.resolve()
+          // }
+        }
+        if (item.status === 'deleted') {
+          if (!command.dryRun) {
+            return api.files
+              .delete({ applicationId: config.applicationId,  fileId: item.id })
+              .then(() => {
+                delete meta[item.file];
+                logResult('deleted', item.file, 'yellow');
                 return Promise.resolve();
               });
-            }
-            logResult('uploaded', item.file, 'green');
-            return Promise.resolve();
           }
-        }));
-      })
-      .then(() => {
-        try { saveLocalMeta('files', meta); } catch (err) { logError(err); }
-      })
-      .catch((e) => {
-        try { saveLocalMeta('files', meta); } catch (err) { logError(err); }
-        logError(e);
-        // console.log(e)
-      });
+          logResult('deleted', item.file, 'yellow');
+          return Promise.resolve();
+        } else {
+          if (!command.dryRun) {
+            let action;
+            const body = fs.readFileSync(item.file);
+            if (item.id) {
+              action = api.file
+                .patch({
+                  applicationId: config.applicationId,
+                  fileId: item.id,
+                  file:  {
+                    fileSize: item.size
+                  }
+                });
+            } else {
+              const pathParts = item.file.split(path.sep);
+              action = api.files
+                .post({
+                  applicationId: config.applicationId,
+                  file: {
+                    name: item.name,
+                    parentDirectory: pathParts.slice(1, -1).join(path.sep),
+                    type: 'file',
+                    fileSize: item.size,
+                    contentType: mimeTypes.lookup(item.file)
+                  }
+                });
+            }
+            return action.then((file) => {
+              return new Promise((resolve, reject) => {
+                const fd = new FormData();
+                Object.keys(file.upload.fields).forEach((key) => {
+                  if (key !== 'bucket') {
+                    fd.append(key, file.upload.fields[key]);
+                  }
+                });
+                fd.append('file', body);
+                fd.submit(file.upload.url, (err, res) => {
+                  // const body = [];
+                  if (err) {
+                    return reject(err);
+                  }
+                  res.on('data', (chunk) => {
+                    body.push(chunk);
+                  });
+                  res.on('end', () => {
+                    return resolve(file);
+                  });
+                });
+              });
+            }).then((file) => {
+              const mtime = new Date(file.lastUpdated);
+              //fs.writeFileSync(item.file, body)
+              meta[item.file] = {
+                id: file.id,
+                md5: checksum(body),
+                remoteTime: mtime.getTime(),
+                localTime: item.localModTime * 1000
+              };
+              logResult('uploaded', item.file, 'green');
+              return Promise.resolve();
+            });
+          }
+          logResult('uploaded', item.file, 'green');
+          return Promise.resolve();
+        }
+      }));
+      try { saveLocalMeta('files', meta); } catch (err) { logError(err); }
+    } catch (error) {
+      try { saveLocalMeta('files', meta); } catch (err) { logError(err); }
+      logError(error);
+    }
   });
 
 program
@@ -269,42 +256,40 @@ program
   .option('-c, --config <file>', 'config file to run the command with')
   .option('-d, --dir <dir>', 'directory to run the command in. (default current directory)')
   .option('-r, --remote', 'show remote file status')
-  .action((command) => {
+  .action(async (command) => {
     if (command.dir) {
       process.chdir(command.dir);
     }
 
     const config = loadConfig(command.config);
     const api = losant.createClient({ accessToken: config.apiToken });
-
-    api.files
-      .get({ applicationId: config.applicationId })
-      .then((files) => {
-        if (command.remote) {
-          // remove directories
-          files.items = files.items.filter((item) => {
-            return item.type === 'file';
-          });
-          const remoteStatus = getRemoteStatus('files', files.items, 'files${parentDirectory}${name}'); // eslint-disable-line no-template-curly-in-string
-          if (remoteStatus.length === 0) {
-            log('No remote files found');
-          }
-          remoteStatus.forEach((item) => {
-            if (item.status === 'added') { logResult(item.status, item.file, 'green'); } else if (item.status === 'modified') { logResult(item.status, item.file, 'yellow'); } else if (item.status === 'deleted') { logResult(item.status, item.file, 'red'); } else { logResult(item.status, item.file); }
-          });
-        } else {
-          const localStatus = getLocalStatus('files', '/**/*.*', 'files');
-          if (localStatus.length === 0) {
-            log('No local files found');
-          }
-          localStatus.forEach((item) => {
-            if (item.status === 'added') { logResult(item.status, item.file, 'green'); } else if (item.status === 'modified') { logResult(item.status, item.file, 'yellow'); } else if (item.status === 'deleted') { logResult(item.status, item.file, 'red'); } else { logResult(item.status, item.file); }
-          });
-        }
-      })
-      .catch((e) => {
-        logError(e);
+    let files;
+    try {
+      files = await api.files.get({ applicationId: config.applicationId }) || [];
+    } catch (err) {
+      logError(err);
+    }
+    if (command.remote) {
+      // remove directories
+      files.items = files.items.filter((item) => {
+        return item.type === 'file';
       });
+      const remoteStatus = getRemoteStatus('files', files.items, 'files${parentDirectory}${name}'); // eslint-disable-line no-template-curly-in-string
+      if (remoteStatus.length === 0) {
+        log('No remote files found');
+      }
+      remoteStatus.forEach((item) => {
+        if (item.status === 'added') { logResult(item.status, item.file, 'green'); } else if (item.status === 'modified') { logResult(item.status, item.file, 'yellow'); } else if (item.status === 'deleted') { logResult(item.status, item.file, 'red'); } else { logResult(item.status, item.file); }
+      });
+    } else {
+      const localStatus = getLocalStatus('files', '/**/*.*', 'files');
+      if (localStatus.length === 0) {
+        log('No local files found');
+      }
+      localStatus.forEach((item) => {
+        if (item.status === 'added') { logResult(item.status, item.file, 'green'); } else if (item.status === 'modified') { logResult(item.status, item.file, 'yellow'); } else if (item.status === 'deleted') { logResult(item.status, item.file, 'red'); } else { logResult(item.status, item.file); }
+      });
+    }
   });
 
 program
