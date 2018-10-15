683a62a4e98d3cce53c40a30ed410194abe5ae1b
Erin <thompsonerin42@gmail.com>
2018-10-15 11:34:24 -0400

    git style nested calls and moving views to expierence views

--- bin/losant.js
+++ bin/losant.js
@@ -8,7 +8,7 @@ program
   .version(pkg.version)
   .description('Losant CLI is a command line tool to aid developers while creating custom application on top of the Losant Platform')
   .command('configure', 'Configure the command line tool')
-  .command('views', 'Manage Experience Views')
+  .command('experience', 'Manage Experience Views')
   .command('files', 'Manage Files')
   .parse(process.argv);
 
