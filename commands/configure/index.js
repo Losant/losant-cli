fc5695c40d9a3f62940e99d355c6dad34aa52a85
Erin <thompsonerin42@gmail.com>
2018-10-17 11:29:37 -0400

    adding s3etag to files md5, and updated the gitignore file

--- commands/configure/index.js
+++ commands/configure/index.js
@@ -3,33 +3,30 @@ const c = require('chalk');
 const { saveConfig, logError, logResult, setDir } = require('../../lib/utils');
 const { options } = require('../../lib/constants');
 
-module.exports = (() => {
-  program
-    .description('Configure the command line tool')
-    .option(...options.directory)
-    .option(...options.config)
-    .option('-a, --app <id>', 'set the application id')
-    .option('-t, --token <token>', 'set the api token')
-    .action(async (command) => {
-      if (!command.app) {
-        logError('application id is required');
-        process.exit(1);
-      }
-      if (!command.token) {
-        logError('api token is required');
-        process.exit(1);
-      }
+program
+  .description('Configure the command line tool')
+  .option(...options.directory)
+  .option(...options.config)
+  .option('-a, --app <id>', 'set the application id')
+  .option('-t, --token <token>', 'set the api token')
+  .action(async (command) => {
+    if (!command.app) {
+      logError('application id is required');
+      process.exit(1);
+    }
+    if (!command.token) {
+      logError('api token is required');
+      process.exit(1);
+    }
 
-      setDir(command);
-      const config = { applicationId: command.app, apiToken: command.token };
-      try {
-        const file = await saveConfig(command.config, config);
-        logResult('success', `configuration written to ${c.bold(file)}`, 'green');
-      } catch (e) {
-        logError(`failed to write configuration: ${c.bold(e.message)}`);
-      }
-    });
+    setDir(command);
+    const config = { applicationId: command.app, apiToken: command.token };
+    try {
+      const file = await saveConfig(command.config, config);
+      logResult('success', `configuration written to ${c.bold(file)}`, 'green');
+    } catch (e) {
+      logError(`failed to write configuration: ${c.bold(e.message)}`);
+    }
+  });
 
-  return program;
-
-})();
+module.exports = program;
