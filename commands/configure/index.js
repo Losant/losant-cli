7870a470967065e22770d3026095a2b171570f0d
Erin <thompsonerin42@gmail.com>
2018-10-17 14:06:55 -0400

    adding application questions to set configuration

--- commands/configure/index.js
+++ commands/configure/index.js
@@ -1,29 +1,49 @@
 const program = require('commander');
+const losant = require('losant-rest');
 const c = require('chalk');
 const { saveConfig, logError, logResult, setDir } = require('../../lib/utils');
 const { options } = require('../../lib/constants');
+const inquirer = require('inquirer');
+const { trim } = require('omnibelt');
 
 program
   .description('Configure the command line tool')
   .option(...options.directory)
-  .option(...options.config)
-  .option('-a, --app <id>', 'set the application id')
-  .option('-t, --token <token>', 'set the api token')
   .action(async (command) => {
-    if (!command.app) {
-      logError('application id is required');
-      process.exit(1);
+    const { email, password, filter } = await inquirer.prompt([
+      { type: 'input', name: 'email', message: 'Enter Losant email' },
+      { type: 'password', name: 'password', message: 'Enter Losant password' },
+      { type: 'input', name: 'filter', message: 'Enter an Application Name' }
+    ]);
+    let token;
+    const api = losant.createClient();
+    try {
+      const results = await api.auth.authenticateUser({ credentials: { email: trim(email), password }, tokenTTL: 0 });
+      token = results.token;
+    } catch (e) {
+      return logError(`Failed to authenticate with Losant: ${e.message}`);
     }
-    if (!command.token) {
-      logError('api token is required');
-      process.exit(1);
+    api.setOption('accessToken', token);
+
+    const applications = await api.applications.get({ filterField: 'name', filter });
+    let applicationId, applicationName;
+    if (!applications.count) {
+      logError(`Failed to find any applications with the name ${filter}`);
+    } else if (applications.count === 1) {
+      applicationId = applications.items[0].id;
+    } else {
+      const nameToId = {};
+      applications.items.forEach(({ id, name }) => { nameToId[name] = id; });
+      const { name } = await inquirer.prompt([ { type: 'list', name: 'name', message: `Several applications were found with the name ${filter}, choose one:`, choices: Object.keys(nameToId) } ]);
+      applicationId = nameToId[name];
+      applicationName = name;
     }
 
     setDir(command);
-    const config = { applicationId: command.app, apiToken: command.token };
+    const config = { applicationId, apiToken: token };
     try {
       const file = await saveConfig(command.config, config);
-      logResult('success', `configuration written to ${c.bold(file)}`, 'green');
+      logResult('success', `configuration written to ${c.bold(file)} for the application ${applicationName}`, 'green');
     } catch (e) {
       logError(`failed to write configuration: ${c.bold(e.message)}`);
     }
