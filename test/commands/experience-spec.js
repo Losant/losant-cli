a95937aac228f82e148a89ff268a95b95d5b13d1
Erin <thompsonerin42@gmail.com>
2018-10-29 12:04:55 -0400

    handling the case if the user configuration was made but empty

--- test/commands/experience-spec.js
+++ test/commands/experience-spec.js
@@ -34,7 +34,7 @@ describe('Experiene Commands', () => {
       'status'
     ]);
     const msg = await deferred.promise;
-    msg.should.equal(errorLog('Configuration file losant.yml does not exist, run losant configure to generate this file.'));
+    msg.should.equal(errorLog('Configuration file missing for this directory, run losant configure to generate this file.'));
   });
   it('should run get status', async () => {
     await buildConfig();
