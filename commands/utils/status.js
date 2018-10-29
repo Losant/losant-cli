65e35d359ca6a77b1a0249febcdeb3c6341821e4
Erin <thompsonerin42@gmail.com>
2018-10-29 11:14:19 -0400

    starting to rework status

--- commands/utils/status.js
+++ commands/utils/status.js
@@ -4,13 +4,10 @@ const printHelp = require('../../lib/print-help');
 module.exports = (program, type, params = {}) => {
   const subProgram = program
     .command('status')
-    .option('-r, --remote', `show remote ${type} status`)
     .action(getStatusFunc(params));
   const helpLines = [
-    'Check local modification status',
-    `$ losant ${type} status`,
-    'Check remote modification status',
-    `$ losant ${type} status -r`
+    'Check status of all experience files',
+    `$ losant ${type} status`
   ];
   printHelp(subProgram, helpLines);
   return { helpLines };
