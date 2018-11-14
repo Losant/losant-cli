7ad5bf4dd670679f14076f34973ba982b8adcf80
Erin <thompsonerin42@gmail.com>
2018-11-14 13:47:06 -0500

    updates to download

--- commands/utils/download.js
+++ commands/utils/download.js
@@ -1,10 +1,16 @@
 const { getDownloader } = require('../../lib');
 const { options } = require('../../lib/constants');
 
-module.exports = (program, params = {}) => {
-  return program
+module.exports = (program, params = {}, opts) => {
+  const subProgram = program
     .command('download [pattern]')
     .option(...options.force)
-    .option(...options.dryRun)
-    .action(getDownloader(params));
+    .option(...options.dryRun);
+
+  if (opts && opts.length) {
+    opts.forEach((opt) => {
+      subProgram.option(...opt);
+    });
+  }
+  return subProgram.action(getDownloader(params));
 };
