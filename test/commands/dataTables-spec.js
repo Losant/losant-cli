98e4c4c052a2567967ec78be399dae17a42230fa
RozziP <rozzipeter@gmail.com>
2019-07-15 14:55:19 -0400

    start on tests

--- test/commands/dataTables-spec.js
+++ test/commands/dataTables-spec.js
@@ -2,9 +2,7 @@ const path = require('path');
 const ssLog = require('single-line-log');
 const {
   sinon,
-  nock,
   errorLog,
-  buildConfig,
   buildUserConfig
 } = require('../common');
 const { defer } = require('omnibelt');
@@ -24,25 +22,5 @@ describe('Data Tables Commands', () => {
     ]);
     const msg = await deferred.promise;
     msg.should.equal(errorLog('Configuration file missing for this directory, run losant configure to generate this file.'));
-  });
-
-  it('should run get status', async function() {
-    await buildConfig();
-    const deferred = defer();
-    nock('https://api.losant.space:443', { encodedQueryParams: true })
-      .get('/applications/5b9297591fefb200072e554d/data-tables')
-      .query({ _actions: 'false', _links: 'true', _embedded: 'true', page: 0, perPage: 1000 })
-      .reply(200, {});
-    sinon.stub(ssLog, 'stdout').callsFake((message) => {
-      deferred.resolve(message);
-    });
-
-    require('../../commands/dataTables').parse([
-      '/bin/node',
-      path.resolve(__dirname, '/bin/losant-dataTables.js'),
-      'status'
-    ]);
-    const msg = await deferred.promise;
-    msg.should.equal('No dataTables found.');
   });
 });
