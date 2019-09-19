0bb2038dc8398a651e68f734ea7035370a34e59e
Erin <thompsonerin42@gmail.com>
2019-09-19 10:42:00 -0400

    in the middle of having everything throw errors to process.exit only in one spot

--- test/commands/dataTables-spec.js
+++ test/commands/dataTables-spec.js
@@ -10,18 +10,22 @@ const {
 const { defer } = require('omnibelt');
 
 describe('Data Tables Commands', () => {
-  it('should log an error if configure was not run first', async function() {
+  it.only('should log an error if configure was not run first', async function() {
     await buildUserConfig();
     const deferred = defer();
     sinon.stub(ssLog, 'stdout').callsFake((message) => {
       deferred.resolve(message);
     });
-
-    require('../../commands/dataTables').parse([
-      '/bin/node',
-      path.resolve(__dirname, '/bin/losant-dataTables.js'),
-      'export'
-    ]);
+    try {
+      require('../../commands/dataTables').parse([
+        '/bin/node',
+        path.resolve(__dirname, '/bin/losant-dataTables.js'),
+        'export'
+      ]);
+    } catch (err) {
+      console.log('caught the error');
+      throw err;
+    }
     const msg = await deferred.promise;
     msg.should.equal(errorLog('Configuration file missing for this directory, run losant configure to generate this file.'));
   });
