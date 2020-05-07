b7d865f1a633736f1884edb64dd04c85979aa2f2
eeerin <erin@losant.com>
2020-05-07 13:52:06 -0400

    ensuring that even unmodified files get name put in the yaml

--- test/lib/get-downloader-spec.js
+++ test/lib/get-downloader-spec.js
@@ -219,6 +219,7 @@ describe('#getDownloader', () => {
     messages[9].should.equal(`${pad(c.green('downloaded'), 13)}\texperience/layouts/${exps[4].id}.hbs`);
   });
   it('should leave files that have previously been created locally', async () => {
+    const localText =  'write something else to make it modified...';
     const exps = [
       {
         name: 'my:View',
@@ -233,13 +234,26 @@ describe('#getDownloader', () => {
         id: '5b92975dc2f8de0006e2ca93',
         _type: 'experienceView',
         _links: { self: { href: '/applications/5b9297591fefb200072e554d/experience/views/5b92975dc2f8de0006e2ca93' } }
+      },
+      {
+        name: 'myOtherView',
+        description: 'description',
+        viewType: 'layout',
+        body: localText,
+        applicationId: '5b9297591fefb200072e554d',
+        creationDate: '2018-09-07T15:21:01.223Z',
+        lastUpdated: '2018-09-07T15:21:01.234Z',
+        viewTags: {},
+        experienceViewId: '5b92975dc2f8de0006e2ca90',
+        id: '5b92975dc2f8de0006e2ca90',
+        _type: 'experienceView',
+        _links: { self: { href: '/applications/5b9297591fefb200072e554d/experience/views/5b92975dc2f8de0006e2ca93' } }
       }
     ];
     nockExperienceView(exps);
 
     const downloader = getDownloader(EXP_DOWNLOAD_PARAMS);
     const command = {};
-    const localText =  'write something else to make it modified...';
     const md5 = crypto.createHash('md5')
       .update(localText, 'utf8')
       .digest('hex');
@@ -254,21 +268,32 @@ describe('#getDownloader', () => {
         md5,
         remoteTime: 1536333661234,
         localTime: 1536333661234
+      },
+      'experience/layouts/myOtherView.hbs': {
+        file: 'experience/layouts/myOtherView.hbs',
+        id: '5b92975dc2f8de0006e2ca90',
+        md5,
+        remoteTime: 1536333661234,
+        localTime: 1536333661234
       }
     });
-    const nextLog = getNextLogs({ numMsg: 1 });
+    const nextLog = getNextLogs({ numMsg: 3 });
     await downloader(null, command);
     await unlockConfigFiles('.application.yml');
     const messages = await nextLog.promise;
     messages[0].should.equal(`${pad(c.gray('processing'), 13)}\texperience/layouts/my:View.hbs`);
     messages[1].should.equal(`${pad(c.green('downloaded'), 13)}\texperience/layouts/my:View.hbs`);
+    messages[2].should.equal(`${pad(c.gray('processing'), 13)}\texperience/layouts/myOtherView.hbs`);
+    messages[3].should.equal(`${pad(c.gray('unmodified'), 13)}\texperience/layouts/myOtherView.hbs`);
     const layout = (await readFile(path.join(path.join(process.env.DIR, 'experience/layouts/my:View.hbs')))).toString();
     layout.should.equal('a body');
 
     const localMeta = await utils.loadLocalMeta('experience');
     Object.keys(localMeta).should.deepEqual([
-      'experience/layouts/my:View.hbs'
+      'experience/layouts/my:View.hbs',
+      'experience/layouts/myOtherView.hbs'
     ]);
     localMeta['experience/layouts/my:View.hbs'].name.should.equal('my:View');
+    localMeta['experience/layouts/myOtherView.hbs'].name.should.equal('myOtherView');
   });
 });
