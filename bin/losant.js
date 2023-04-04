04545e2c38c4ae530d08111f476223f5f626a168
eeerin <thompsonerin42@gmail.com>
2023-04-04 14:03:27 -0400

    small tweak

--- bin/losant.js
+++ bin/losant.js
@@ -12,9 +12,7 @@ const pgm = program
   .command('configure', 'Configure the command line tool for a specific directory.')
   .command('experience', 'Manages your Losant Application\'s Experience Views, and Versions from the command line.')
   .command('files', 'Manage Files on Losant for your Application.')
-  .command('datatables', 'Manage Data Tables on Losant for your Application.')
-  .storeOptionsAsProperties()
-  .showHelpAfterError();
+  .command('datatables', 'Manage Data Tables on Losant for your Application.');
 
 pgm.parse(process.argv);
 
