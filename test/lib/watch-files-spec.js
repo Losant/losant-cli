c8419abe205781c0725ad9106a2a5be8532b7612
eeerin <thompsonerin42@gmail.com>
2023-04-14 13:39:27 -0400

    updating watch test

--- test/lib/watch-files-spec.js
+++ test/lib/watch-files-spec.js
@@ -8,12 +8,13 @@ const {
 const { defer, sleep } = require('omnibelt');
 const watch = require('../../lib/watch-files')('files', 1000);
 const ssLog = require('single-line-log');
-const { ensureDir, writeFile, appendFile } = require('fs-extra');
+const { ensureDir, writeFile, appendFile, emptyDir } = require('fs-extra');
 
 describe('#Watch Files', () => {
   let watcherClose;
   beforeEach(async () => {
     await buildConfig();
+    await emptyDir('files');
     await ensureDir('files/mine');
   });
 
@@ -25,21 +26,18 @@ describe('#Watch Files', () => {
   });
 
   it('should process files in the order they were queued', async function() {
-    await Promise.all([
-      writeFile('files/help.txt', 'hello'),
-      writeFile('files/yo.txt', 'hello'),
-      writeFile('files/mine/myFile.txt', 'hello')
-    ]);
+    await writeFile('files/help.txt', 'hello');
+    await writeFile('files/yo.txt', 'hello');
+    await writeFile('files/mine/myFile.txt', 'hello');
     watcherClose = await watch();
     this.timeout(30000);
-    for (let i = 0; i < 3; i++) {
-      nock('https://api.losant.com:443', { encodedQueryParams: true })
-        .get('/applications/5b9297591fefb200072e554d/files')
-        .query({
-          _actions: 'false', _links: 'true', _embedded: 'true', type: 'file', page: 0, perPage: 1000
-        })
-        .reply(200, {});
-    }
+    nock('https://api.losant.com:443', { encodedQueryParams: true })
+      .get('/applications/5b9297591fefb200072e554d/files')
+      .query({
+        _actions: 'false', _links: 'true', _embedded: 'true', type: 'file', page: 0, perPage: 1000
+      })
+      .times(3)
+      .reply(200, {});
     nock('https://api.losant.com:443', { encodedQueryParams: true })
       .post('/applications/5b9297591fefb200072e554d/files', { name: 'help.txt', parentDirectory: '/', type: 'file', fileSize: 9, contentType: 'text/plain' })
       .query({ _actions: 'false', _links: 'true', _embedded: 'true' })
@@ -115,20 +113,19 @@ describe('#Watch Files', () => {
         }
       });
 
-    for (let i=0; i < 3; i++) {
-      nock('https://s3.us-west-2.amazonaws.com:443', { encodedQueryParams: true })
-        .post('/files.onlosant.com', /.*/)
-        .reply(204, '', [ 'x-amz-id-2',
-          'bgtyS9TMwD4MWmaWcRFviQutaZKHdQWv1icmHjVkUeUYByunN4Ajmti3L2Rz0AH2tHJs0CR6y5c=',
-          'x-amz-request-id',
-          '9BC74EBBBFE8D047',
-          'Date',
-          'Fri, 19 Oct 2018 20:24:20 GMT',
-          'ETag',
-          '"5eb63bbbe01eeed093cb22bb8f5acdc3"',
-          'Location',
-          'https://s3.us-west-2.amazonaws.com/files.onlosant.com/5b9297591fefb200072e554d%2FnewFile.txt']);
-    }
+    nock('https://s3.us-west-2.amazonaws.com:443', { encodedQueryParams: true })
+      .post('/files.onlosant.com', /.*/)
+      .times(3)
+      .reply(204, '', [ 'x-amz-id-2',
+        'bgtyS9TMwD4MWmaWcRFviQutaZKHdQWv1icmHjVkUeUYByunN4Ajmti3L2Rz0AH2tHJs0CR6y5c=',
+        'x-amz-request-id',
+        '9BC74EBBBFE8D047',
+        'Date',
+        'Fri, 19 Oct 2018 20:24:20 GMT',
+        'ETag',
+        '"5eb63bbbe01eeed093cb22bb8f5acdc3"',
+        'Location',
+        'https://s3.us-west-2.amazonaws.com/files.onlosant.com/5b9297591fefb200072e554d%2FnewFile.txt']);
 
     const deferred = defer();
     const messages = [];
@@ -139,11 +136,11 @@ describe('#Watch Files', () => {
       }
     });
     await appendFile('files/help.txt', ' mom');
-    await sleep(150);
+    await sleep(100);
     await appendFile('files/yo.txt', ' dad');
-    await sleep(150);
+    await sleep(200);
     await appendFile('files/mine/myFile.txt', ' son');
-    await sleep(150);
+    await sleep(300);
     await deferred.promise;
     messages.length.should.equal(10);
     const queueing = messages.slice(0, 3);
