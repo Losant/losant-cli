b964ec26247901681df644fe81a952a32744caaf
eeerin <thompsonerin42@gmail.com>
2023-10-27 15:51:16 -0400

    updating set token to take an optional token as part of the command

--- test/commands/set-token-spec.js
+++ test/commands/set-token-spec.js
@@ -68,4 +68,57 @@ describe('Set Token Command', () => {
     });
 
   });
+
+  it('should set the API token without being prompted', async () => {
+    nock('https://api.losant.com:443', { encodedQueryParams: true })
+      .get('/whitelabels/domain')
+      .reply(200,
+        {
+          appUrl: 'https://app.losant.com',
+          endpointDomain: 'endpoint.onlosant.com'
+        },
+        [ 'Date',
+          'Mon, 10 May 2021 14:42:04 GMT',
+          'Content-Type',
+          'application/json',
+          'Content-Length',
+          '56',
+          'Connection',
+          'close',
+          'Server',
+          'Losant API',
+          'Pragma',
+          'no-cache',
+          'Cache-Control',
+          'no-cache, no-store, must-revalidate',
+          'X-Content-Type-Options',
+          'nosniff',
+          'X-XSS-Protection',
+          '1; mode=block',
+          'Content-Security-Policy',
+          'default-src \'none\'; style-src \'unsafe-inline\'',
+          'Access-Control-Allow-Origin',
+          '*',
+          'Strict-Transport-Security',
+          'max-age=31536000'
+        ]
+      );
+
+    require('../../commands/set-token').parse([
+      '/bin/node',
+      path.resolve(__dirname, '/bin/set-token.js'),
+      'token1'
+    ]);
+
+    await sleep(500); // give it a second to right the file...
+    const config = await utils.loadUserConfig(false);
+    config.should.deepEqual({
+      'https://api.losant.com': {
+        apiToken: 'token1',
+        appUrl: 'https://app.losant.com',
+        endpointDomain: 'endpoint.onlosant.com'
+      }
+    });
+
+  }).timeout(30000);
 });
